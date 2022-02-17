1f3ab555afe9f1eccb362bb78475b753af153a9e
Jordan Bravo <jbravo0423@gmail.com>
2022-02-17 00:17:51 -0500

    second commit

---
+++ 13-npmCommands.js
@@ -0,0 +1,16 @@
+// npm (global command, comes with node)
+// npm --version (version of npm)
+
+// local dependency (use it in this project only)
+// npm i <packageName>
+
+// global dependency (use it in any project)
+// npm i -g <packageName>
+
+// package.json (manifest file that stores important info about project/package)
+
+const _ = require('lodash')
+
+const items = [ 1, [ 2, [ 3, [ 4 ] ] ] ]
+const newItems = _.flattenDeep(items)
+console.log(newItems);
